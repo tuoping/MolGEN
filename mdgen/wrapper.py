43a29a7cf9f316cf4d83e75ed337bad09aae331f
tuoping <tuop1994@gmail.com>
2025-08-23 21:31:10 -0700

    change LR schedules

--- mdgen/wrapper.py
+++ mdgen/wrapper.py
@@ -167,14 +167,14 @@ class Wrapper(pl.LightningModule):
 
     def configure_optimizers(self):
         if not self.args.lr_decay:
-            opt = torch.optim.AdamW(self.parameters(), lr=5e-5)
+            opt = torch.optim.AdamW(self.parameters(), lr=1e-4)
             return opt
         else:
             # linear warmup to 1e-4 over W epochs, then cosine back to 3e-5
             W = 5  # warmup epochs
-            max_lr = 1e-4
+            max_lr = 3e-4
             T = 600 + W  # cosine length (adjust)
-            base, min_lr = 5e-5, 5e-7
+            base, min_lr = 1e-4, 5e-7
             if W < 1: max_lr = base
         
             opt = torch.optim.AdamW(self.parameters(), lr=base)
