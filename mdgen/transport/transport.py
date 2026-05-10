130a99021fe49dfadb4ed37b87bdf5da2592c204
tuoping <tuop1994@gmail.com>
2026-05-10 09:26:17 -0700

    merge

--- mdgen/transport/transport.py
+++ mdgen/transport/transport.py
@@ -399,16 +399,6 @@ class Transport:
         ### normal sampler of t
         t, x0, x0_mean = self.sample(x1.shape, x1.device)
         ### OT in the atom number dimension
-<<<<<<< HEAD
-        x1 = x1.view(B*T, N, C)
-        model_kwargs['x1'] = model_kwargs['x1'].view(B*T, N, C)
-        assignment = hungarian_over_L(th.cdist(x0[0].view(B*T, N, C), x1))
-        x1 = x1[th.arange(B).unsqueeze(1).expand(B, N), assignment]
-        model_kwargs['x1'] = model_kwargs['x1'][th.arange(B).unsqueeze(1).expand(B, N), assignment]
-        x1 = x1.view(B, T, N, C)
-        model_kwargs['x1'] = model_kwargs['x1'].view(B, T, N, C)
-
-=======
         if self.prior_mean is None:
             x1 = x1.view(B*T, N, C)
             model_kwargs['x1'] = model_kwargs['x1'].view(B*T, N, C)
@@ -418,7 +408,6 @@ class Transport:
             x1 = x1.view(B, T, N, C)
             model_kwargs['x1'] = model_kwargs['x1'].view(B, T, N, C)
         
->>>>>>> e85296f72ca06b130cfb81a9d76995bbc94df339
         ### OT in the batch dimension
         # # Flatten each sample's atom features into a single vector: (B*T, N*C)
         # x0_flat = x0[0].view(B*T, N, C).reshape(B*T, N*C)
